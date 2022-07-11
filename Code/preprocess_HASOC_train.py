4b96f09cb7ff5c660225b80b61a35c810bd122e5
Michiel van Nederpelt <61193709+MichielvNederpelt@users.noreply.github.com>
2022-07-11 14:34:12 +0200

    Update preprocess_HASOC_train.py

--- Code/preprocess_HASOC_train.py
+++ Code/preprocess_HASOC_train.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-inputfile_train = "/Users/michielv.nederpelt/Desktop/Studie/Thesis/data/HASOC/hasoc2019_en_test-2919.tsv"
-output_file = "/Users/michielv.nederpelt/Desktop/Studie/Thesis/data/HASOC/HASOC_SOURCE_english_dataset_test_preprocessed.tsv"
+inputfile_train = "data/HASOC/hasoc2019_en_test-2919.tsv"
+output_file = "data/HASOC/HASOC_SOURCE_english_dataset_test_preprocessed.tsv"
 path_to_file = inputfile_train
 df = pd.read_csv(path_to_file, sep="\t")
 df = df.drop(columns="task_2")
